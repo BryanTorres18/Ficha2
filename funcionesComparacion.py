ae6095cab6021129d042bc971e7eb134228d8019
BryanTorres18 <90003001+BryanTorres18@users.noreply.github.com>
2021-09-02 14:31:58 -0500

    Create funcionesComparacion.py

---
+++ funcionesComparacion.py
@@ -0,0 +1,14 @@
+# Modulo Comparaciones
+def comparar(num1, num2):
+    if num1 == num2:
+        print('Ambos valores son iguales')
+    else:
+        print('Son diferentes')
+    if num1 > num2:
+        print(f'El mayor es {num1}')
+    else:
+        print(f'El mayor es {num2}')
+    if num1 > num2:
+        print(f'El menor es {num2}')
+    else:
+        print(f'El menor es {num1}')
